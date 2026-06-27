b700b9a4e423dddf9c90ea5a72d6730e51e4948d
adivcbot-ship-it <adivcbot@gmail.com>
2026-06-27 13:25:33 +0530

    Create __init__.py

---
+++ config/__init__.py
@@ -0,0 +1,20 @@
+"""
+VCFight V2 - Configuration Module
+"""
+
+from .settings import *
+
+__all__ = [
+    'API_ID',
+    'API_HASH', 
+    'BOT_TOKEN',
+    'SUDO_USERS',
+    'DEFAULT_RADIO_URL',
+    'AUDIO_BITRATE',
+    'AUDIO_CHANNELS',
+    'SCREENSHARE_QUALITY',
+    'SCREENSHARE_FPS',
+    'DATABASE_URL',
+    'LOG_LEVEL',
+    'TEMP_DIR'
+]
